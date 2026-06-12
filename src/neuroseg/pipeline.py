68afa73079c76cfc40d46ca19bf03f21ebc16671
Mohammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2026-06-12 18:01:40 +0200

    change the visualization technique

--- src/neuroseg/pipeline.py
+++ src/neuroseg/pipeline.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from langchain_core.runnables.graph import MermaidDrawMethod
 from langgraph.graph import StateGraph, START, END
 from neuroseg.models.state import State
 from neuroseg.models.node import Node
@@ -70,6 +69,4 @@ def run(data_dir: str | Path, mode: Mode = Mode.INFERENCE):
 
 def visualize_pipeline(output_path: str | Path = "docs/pipeline.png"):
     app = build_app()
-    png_bytes = app.get_graph().draw_mermaid_png(draw_method=MermaidDrawMethod.PYPPETEER)
-    with open(output_path, "wb") as f:
-        f.write(png_bytes)
+    app.get_graph().draw_png(str(output_path))
